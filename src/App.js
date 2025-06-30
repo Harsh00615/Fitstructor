e3fcb37c672d85ceee42e1a39eceab6862088f13
Harsh <harahkhera00615@gmail.com>
2025-06-30 18:14:17 +0530

    just checking

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import Footer from './components/Footer/Footer';
 import { Navbar } from './components/Navbar/Navbar';
 import Home from './pages/CSS/Home';
-
+import { Check } from './pages/check';
 
 function App() {
   return (
     <>
      <Navbar/>
      <Home/>
+     <Check/>
      <Footer/>
     </>
   );
