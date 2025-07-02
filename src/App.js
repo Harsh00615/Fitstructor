27a8b91ee8c3f19fc85482e5a4e7b9236a1aab48
Asees <asees2910kaur@gmail.com>
2025-07-02 12:50:40 +0530

    deleting plant

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import Footer from './components/Footer/Footer';
 import { Navbar } from './components/Navbar/Navbar';
 import Home from './pages/CSS/Home';
-import Plant from'./pages/CSS/Plant';
 
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
     <>
      <Navbar/>
      <Home/>
-     <Plant/>
      <Footer/>
     </>
   );
